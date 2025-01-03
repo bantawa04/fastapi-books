2b3588364ac59999c51eada4e397a1b8469047ea
Pawan <rahunn3@gmail.com>
2025-01-03 13:01:47 +0545

    fix:update response model for get book

--- src/books/routes.py
+++ src/books/routes.py
@@ -20,8 +20,8 @@ async def create_book(book_data:CreateBookRequest,session:AsyncSession = Depends
     new_book = await book_service.create_book(book_data, session)
     return new_book
     
-@book_router.get('/{book_uid}')
-async def get_book(book_uid: int,session:AsyncSession = Depends(get_session)) -> dict:
+@book_router.get('/{book_uid}', response_model=Book)
+async def get_book(book_uid: str,session:AsyncSession = Depends(get_session)) -> dict:
     book = await book_service.get_book(book_uid, session)
     
     if book:
@@ -31,7 +31,7 @@ async def get_book(book_uid: int,session:AsyncSession = Depends(get_session)) ->
 
 
 @book_router.patch('/{book_uid}')
-async def update_book(book_uid:int, book_update: UpdateBookRequest, session:AsyncSession = Depends(get_session))->dict:
+async def update_book(book_uid:str, book_update: UpdateBookRequest, session:AsyncSession = Depends(get_session))->dict:
     update_book = await book_service.update_book(book_uid, book_update, session)
     
     if update_book:
@@ -40,7 +40,7 @@ async def update_book(book_uid:int, book_update: UpdateBookRequest, session:Asyn
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Book not found')            
 
 @book_router.delete('/{book_uid}', status_code=status.HTTP_204_NO_CONTENT)    
-async def delete_book(book_uid:int, session:AsyncSession = Depends(get_session)):
+async def delete_book(book_uid:str, session:AsyncSession = Depends(get_session)):
     
     book_to_delete = await book_service.delete_book(book_uid, session)
     
